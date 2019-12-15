a483d6f78e824ac255fdad44d8ae894b528e1fc9
Ridwan22XD <mre22xd@gmail.com>
2019-12-16 03:41:52 +0700

    Menambah Coding Select Level

---
+++ Assets/Scripts/Coba/MoveStage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Deklarasi untuk mengambil index scene
+using UnityEngine.SceneManagement;
+
+public class MoveStage : MonoBehaviour
+{
+    public int nextSceneLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Menset nextSceneLoad bernilai lebih 1 dari index pada build project
+        nextSceneLoad = SceneManager.GetActiveScene().builIndex + 1;
+    }
+
+    // Update is called once per frame
+    public void OnTriggerEnter(Collider other)
+    {
+        // Fungsi untuk mengecek apakah bertabrakan dengan collider player
+        if(other.gameObject.tag == "Player")
+        {
+            // Jika index level pada project build hanya bernilai 8
+            if(SceneManager.GetActiveScene().buildIndex == 8)
+            {
+                Debug.Log("Silankan Pindah World");
+            }
+
+            else
+            {
+                // Berpindah ke stage selanjutnya
+                SceneManager.LoadScene(nextSceneLoad);
+
+                // Mengecek nilai nextSceneLoad dengan PlayerPrefs
+                if(nextSceneLoad > PlayerPrefs.GetInt("LevelAt"))
+                {
+                    // Set LevelAt bernilai sama dengan nextSceneLoad
+                    PlayerPrefs.SetInt("LevelAt",nextSceneLoad);
+                }
+            }
+            
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Coba/SelectLevel.cs
@@ -0,0 +1,33 @@
+﻿// code ini digunakan untuk menentukkan level
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Deklarasi library
+using UnityEngine.UI;
+
+
+public class SelectLevel : MonoBehaviour
+{
+    // array button untuk setiap level
+    public Button[] lvlButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Misal set level pertama di index ke 2 sesuai index pada build setting
+        // Menggunakan Playerprefs agar nilai tersimpan
+        int levelAt = PlayerPrefs.GetInt("Level",2);
+
+        // fungsi menonaktifkan stage yang levelnya lebih besar dari level pemain
+        for(int i = 0; i < lvlButton.Length; i++)
+        {
+            if(i + 2 > levelAt)
+            {
+                // fungsi menonaktifkan button
+                lvlButton[i].interactable = false;
+            }
+        }
+    }
+
+}
